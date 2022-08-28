685daece71eac856ed70d2fbf6ab0c371c03c6a4
xTalAlex <talale93@hotmail.it>
2022-08-28 04:37:00 +0200

    Update battlefactory.php

--- config/battlefactory.php
+++ config/battlefactory.php
@@ -5,8 +5,8 @@ return [
     'url' => 'https://battlefactory.netlify.app',
 
     'squad_ranks' => [
-        'elite' => 'Elite',
-        'expert'  => 'Expert',
+        'veteran' => 'Veteran',
+        'great'  => 'Great',
         '1200'  => 'Master 1200',
         '1400'  => 'Master 1400',
         '1800'  => 'Master 1800'
