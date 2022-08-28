1eceb63ae3f316ccbc9058da7081cda986811680
xTalAlex <talale93@hotmail.it>
2022-08-28 04:42:05 +0200

    Update battlefactory.php

--- config/battlefactory.php
+++ config/battlefactory.php
@@ -6,7 +6,7 @@ return [
 
     'squad_ranks' => [
         'veteran' => 'Veteran',
-        'great'  => 'Great',
+        'ultra'  => 'Ultra',
         '1200'  => 'Master 1200',
         '1400'  => 'Master 1400',
         '1800'  => 'Master 1800'
