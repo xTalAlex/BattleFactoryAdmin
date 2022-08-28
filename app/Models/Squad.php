f97a537b7e9d9d8f4591ec1f21463a2700ecb10b
xTalAlex <talale93@hotmail.it>
2022-08-28 09:48:10 +0200

    Revert "Update Squad.php"
    This reverts commit ac17a6c1e008e673c46e82118b4c58c72f6f2cdb.

--- app/Models/Squad.php
+++ app/Models/Squad.php
@@ -114,7 +114,7 @@ class Squad extends Model
 
     public function countryName()
     {
-        return $this->country ? country($this->country)->getNativeName() : null;
+        return $this->country ? country($this->country)->getName() : null;
     }
 
     public function nameWords()
